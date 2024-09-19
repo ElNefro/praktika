322db410766de992b8be5a9269a08a05f2ec1e9c
se1fa <se1fa@SE1FA1>
2024-09-19 14:03:20 +0500

    Добавьте файлы проекта.

---
+++ praktika/praktika.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    int num1, num2;
+    std::string z;
+
+    std::cout << "\t Калькулятор\n";
+    std::cout << "\n";
+    std::cout << "Введите первое число: ";
+    std::cin >> num1;
+    std::cout << "Введите второе число: ";
+    std::cin >> num2;
+    std::cout << "Введите знак: ";
+    std::cin >> z;
+
+    if (z == "/" && num2 == 0)
+    {
+        std::cout << "НА НОЛЬ ДЕЛИТЬ НЕЛЬЗЯЯЯ!!!!";
+        return 1;
+    }
+
+    if (z == "+")
+    {
+        std::cout << num1 << " + " << num2 << " = " << num1 + num2;
+    }
+    else if (z == "-")
+    {
+        std::cout << num1 << " - " << num2 << " = " << num1 - num2;
+    }
+    else if (z == "/")
+    {
+        std::cout << num1 << " / " << num2 << " = " << num1 / num2;
+    }
+    else if (z == "*")
+    {
+        std::cout << num1 << " * " << num2 << " = " << num1 * num2;
+    }
+    else if (z == "%")
+    {
+        std::cout << num1 << " % " << num2 << " = " << (num1/100) * num2;
+    }
+
+    return 0;
+}
+
+
